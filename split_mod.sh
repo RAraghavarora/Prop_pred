5a1e8db20c4451426ffebc89c9e56ca73897b26d
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2022-01-20 14:38:21 +0100

    CPU for split

--- split_mod.sh
+++ split_mod.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 #SBATCH --time=96:00:00
-#SBATCH --partition=gpu2                        # specify ml partition or gpu2 partition
-#SBATCH --gres=gpu:2                      # use 1 GPU per node (i.e. use one GPU per task)
-#SBATCH --nodes=1                        # request 1 node
+#SBATCH --partition=haswell                        # specify ml partition or gpu2 partition
+#SBATCH --nodes=2                        # request 1 node
 #SBATCH --ntasks=8
 #SBATCH -J split-ra
-#SBATCH --output=split.out
-#SBATCH --error=split.err
+#SBATCH --output=slatm.out
+#SBATCH --error=slatm.err
 #SBATCH -A p_biomolecules
 #SBATCH --mail-type=all
 #SBATCH        --mail-user=leonardo.medrano@nano.tu-dresden.de
-#SBATCH --mem-per-gpu=8000MB
+#SBATCH --mem-per-cpu=8000MB
 ulimit -s unlimited
 echo Starting Program
 module purge                                 # purge if you already have modules loaded
 module load modenv/scs5
 module load Python
-. ../env/bin/activate
-module load cuDNN/8.0.4.30-CUDA-11.1.1
+. /home/medranos/vdftb20/bin/activate
 echo "training starts"
 walltime=$(squeue -h -j $SLURM_JOBID -o "%L")
 IFS=- read daysleft rest <<< "$walltime"
