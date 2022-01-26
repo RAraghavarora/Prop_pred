932ee60fe61811ecb5a55c6c25c7b7da632e03d8
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2022-01-26 08:38:46 +0100

    a

--- kmod.sh
+++ kmod.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 #SBATCH --time=96:00:00
-#SBATCH --partition=gpu2                        # specify ml partition or gpu2 partition
-#SBATCH --gres=gpu:1                      # use 1 GPU per node (i.e. use one GPU per task)
-#SBATCH --nodes=4                        # request 1 node
-#SBATCH --ntasks=8
-#SBATCH -J kernel
-#SBATCH --output=kdip.out
-#SBATCH --error=kdip.err
+#SBATCH --partition=haswell
+#SBATCH -J bob
+#SBATCH --output=kbob.out
+#SBATCH --error=kbob.err
 #SBATCH -A p_biomolecules
+#SBATCH -N 2
+
+#SBATCH -n 8
+#SBATCH --ntasks-per-node=4
 #SBATCH --mail-type=all
 #SBATCH        --mail-user=reepicheep_logs@protonmail.com
-#SBATCH --mem-per-gpu=8000MB
+#SBATCH --mem-per-cpu=8000MB
+
+
 ulimit -s unlimited
 echo Starting Program
 module purge                                 # purge if you already have modules loaded
