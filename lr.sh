7ae04e7b088aa81c57a2d2012481f3fb66944780
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2022-01-26 15:49:55 +0100

    Linear Regression

---
+++ lr.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --time=96:00:00
+#SBATCH --partition=gpu2                        # specify ml partition or gpu2 partition
+#SBATCH --gres=gpu:2                      # use 1 GPU per node (i.e. use one GPU per task)
+#SBATCH --nodes=16                        # request 1 node
+#SBATCH --ntasks=8
+#SBATCH -J lr
+#SBATCH --output=lr.out
+#SBATCH --error=lr.err
+#SBATCH -A p_biomolecules
+#SBATCH --mail-type=all
+#SBATCH        --mail-user=reepicheep_logs@protonmail.com
+#SBATCH --mem-per-gpu=8000MB
+ulimit -s unlimited
+echo Starting Program
+module purge                                 # purge if you already have modules loaded
+module load modenv/scs5
+module load Python/3.6.4-intel-2018a
+. /home/medranos/vdftb20/bin/activate
+module load cuDNN/8.0.4.30-CUDA-11.1.1
+
+work=/scratch/ws/1/medranos-DFTBprojects/raghav/Prop_pred
+python3 $work/lr_cm.py & python3 $work/lr_bob.py & mkdir testing
+
+echo "training is over :-)"
+EXTSTAT=$?
+exit $EXTSTAT
