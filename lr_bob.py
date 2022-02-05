60bb05e61cb9cd808c060035b63edf8fc6c5ebbd
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2022-02-05 10:47:33 +0100

    lr

--- lr_bob.py
+++ lr_bob.py
@@ -137,9 +137,9 @@ def prepare_data(op):
         p7b.append(p7[nn1])
         p8b.append(p8[nn1])
         p9b.append(p9[nn1])
-        p10b.append(p10[nn1])
-        p11b.append(p11[nn1])
-        TPROP2.append(TPROP[nn1])
+        p10b.append(p10[nn1].numpy())
+        p11b.append(p11[nn1].numpy())
+        TPROP2.append(TPROP[nn1].numpy())
 
     p11b = complete_array(p11b)
 
