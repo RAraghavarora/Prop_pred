43efb135fdab5f7e51975c572ed90dfff6a4a427
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2022-03-09 13:45:45 +0100

    lh

--- plot_learn_history.py
+++ plot_learn_history.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 # Plot the curve of learning rate
 
-lhis = open('only_dft/egap/30000/learning-history.dat', 'r')
+lhis = open('withdft/split/eq/20000/learning-history.dat', 'r')
 
 lines = lhis.readlines()
 x = []
@@ -14,7 +14,21 @@ for line in lines:
     x.append(int(epoch))
     y.append(float(mae))
 
+lhis = open('withdft/slatm/eq/30000/learning-history.dat', 'r')
+
+lines = lhis.readlines()
+x1 = []
+y1 = []
+
+
+for line in lines:
+    epoch, lr, loss, mae = line.split()
+    x1.append(int(epoch))
+    y1.append(float(mae)*23.0621)
+
 plt.plot(x[1500:], y[1500:], '.')
+plt.plot(x1[1500:], y1[1500:], '.')
+
 plt.ylabel("Validation MAE")
 plt.xlabel("Epoch")
 plt.title('Learning history for conv with size of 8000')
