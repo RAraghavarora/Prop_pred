5de8353d794b96016401cd6d3132d4cf55e6a3a9
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2022-03-23 13:41:03 +0100

    Reduce mem

--- slatm_mod2.sh
+++ slatm_mod2.sh
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=4
 #SBATCH --mail-type=all
 #SBATCH        --mail-user=reepicheep_logs@protonmail.com
-#SBATCH --mem-per-cpu=16GB
+#SBATCH --mem-per-cpu=8GB
 #SBATCH --gres=gpu:2                      # use 1 GPU per node (i.e. use one GPU per task)
 
 
