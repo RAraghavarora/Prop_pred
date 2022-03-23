542b59b10b60f6bf3db06d654c0b14db07ca5180
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2022-03-23 18:15:17 +0100

    DataParallel

--- egap_split_bob.py
+++ egap_split_bob.py
@@ -351,7 +351,7 @@ def fit_model_dense(n_train, n_val, n_test, iX, iY, patience):
     if torch.cuda.is_available():
         device = "cuda:0"
     model = NeuralNetwork().to(device)
-    model = nn.parallel.DistributedDataParallel(model)
+    model = nn.DataParallel(model)
     model.to(device)
     loss_fn = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
