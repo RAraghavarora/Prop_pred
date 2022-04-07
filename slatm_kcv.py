9f1dc4fc295768b8d878ee51d6f0e7a999cbaeb1
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2022-04-07 15:35:35 +0200

    Let it simultaneously run for 2k-8k

--- slatm_kcv.py
+++ slatm_kcv.py
@@ -485,8 +485,8 @@ def fit_model_dense(n_train, n_val, n_test, iX, iY, patience, split):
     )
 
 
-train_set = [500, 1000, 2000, 4000, 8000]
-splits = [22, 10, 10, 8, 4]
+train_set = [2000, 4000, 8000]
+splits = [10, 8, 4]
 op = 'EAT'
 n_val = 6000
 
