ad2b1e869997847281cb74af958b67d3efd2db71
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2022-04-13 16:38:41 +0200

    Re run on distorted for more patience of EarlyStopping

--- slatm_kcv.py
+++ slatm_kcv.py
@@ -310,7 +310,7 @@ class EarlyStopping():
     certain epochs.
     """
 
-    def __init__(self, patience=1000, min_delta=0.001):
+    def __init__(self, patience=7000, min_delta=0.001):
         """
         :param patience: how many epochs to wait before stopping when loss is
                not improving
@@ -485,7 +485,7 @@ def fit_model_dense(n_train, n_val, n_test, iX, iY, patience, split):
     )
 
 
-train_set = [30000]
+train_set = [25000, 30000]
 splits = [1]
 op = 'EAT'
 n_val = 6000
