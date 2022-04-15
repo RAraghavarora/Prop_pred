6061d2772412948f5ff0949e2838306d9e8865f5
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2022-04-15 18:42:58 +0200

    changes

--- mod_eq_egap.sh
+++ mod_eq_egap.sh
@@ -48,7 +48,7 @@ echo "training starts"
 #export DFTB_COMMAND='mpiexec -n 1 /home/medranos/vdftb20/dftb/bin/dftb+'
 #export DFTB_PREFIX='/home/medranos/SK-files/3ob-3-1/'
 
-work=/scratch/ws/1/medranos-DFTBprojects/raghav/Prop_pred
+work=/scratch/ws/1/medranos-TUDprojects/raghav/Prop_pred
 python3 $work/train.py EGAP eq withdft/slatm/egap/eq/
 
 echo "training is over :-)"
